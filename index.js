db4129c04eb5bd26e9c8dd61878cb0f2d16b28b0
Jeremiah <jeremiah.r29q7.tabb@gmail.com>
2021-04-03 20:40:49 -0400

    Make more readable

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
 
-let totalBatteries = batteryBatches.reduce( (r,et) => r + et, 0 );
+// More Readable Version 02
+let totalBatteries = batteryBatches.reduce( (sum, element) => 
+    sum + element);
+
+// // Version 01
+// let totalBatteries = batteryBatches.reduce( (r,et) => r + et, 0 );
